6f652cd00cb55e0f1f6f6e96ce0d2b5c2b9c8a08
xrh999 <xrh999@gmail.com>
2026-09-09 07:02:31 +0800

    split longer plugin configs into their own files

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,3 +5,6 @@ end)
 
 -- window switching keybing
 vim.keymap.set("n", "<leader>w", "<C-w>")
+
+-- hide the highlights after pressing esc after searching
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
